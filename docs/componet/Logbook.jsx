77bdd2565cccaaae3a216396167995a8ceca8a7d
AuroraPixel <wanghanlinlin@126.com>
2024-07-10 11:35:47 +0800

    complate-logbook

---
+++ docs/componet/Logbook.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './css/Logbook.css';
+
+const Logbook = ({ children }) => {
+  return (
+    <div className="logbook-container">
+      <h1 className="logbook-title">Daily Log</h1>
+      <ul className="task-list">
+        {React.Children.map(children, (child, index) => {
+          if (child.type === TaskItem) {
+            return React.cloneElement(child, { key: index });
+          }
+          return null;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+const TaskItem = ({ children, isComplete }) => {
+  return (
+    <li className={`task-item ${isComplete ? 'completed' : ''}`}>
+      <input type="checkbox" checked={isComplete} readOnly />
+      {children}
+    </li>
+  );
+};
+
+TaskItem.propTypes = {
+  isComplete: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+};
+
+Logbook.TaskItem = TaskItem;
+
+export default Logbook;
